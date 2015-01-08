3bbe3e777825806eafc1be491914e3d81ab54fb0
Santosh <sastagi@gmail.com>
2015-01-08 15:51:48 -0800

    Created sections for the navigation drawer

--- app/src/main/java/com/autecho/dcc/NavigationDrawerFragment.java
+++ app/src/main/java/com/autecho/dcc/NavigationDrawerFragment.java
@@ -102,9 +102,11 @@ public class NavigationDrawerFragment extends Fragment {
                 android.R.layout.simple_list_item_activated_1,
                 android.R.id.text1,
                 new String[]{
-                        getString(R.string.title_section1),
-                        getString(R.string.title_section2),
-                        getString(R.string.title_section3),
+                        getString(R.string.feed),
+                        getString(R.string.pictures),
+                        getString(R.string.video),
+                        getString(R.string.map),
+                        getString(R.string.dashboard)
                 }));
         mDrawerListView.setItemChecked(mCurrentSelectedPosition, true);
         return mDrawerListView;
