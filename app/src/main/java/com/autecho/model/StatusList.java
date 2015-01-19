3b4d44de2012bdb2d67fecacf5e6a5cf9ae7f671
Santosh <sastagi@gmail.com>
2015-01-18 23:22:36 -0800

    Updated statuslist data structure

--- app/src/main/java/com/autecho/model/StatusList.java
+++ app/src/main/java/com/autecho/model/StatusList.java
@@ -4,6 +4,38 @@ package com.autecho.model;
  * Created by Santosh on 1/14/15.
  */
 public class StatusList {
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getStatuses() {
+        return statuses;
+    }
+
+    public void setStatuses(String statuses) {
+        this.statuses = statuses;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
     private String id;
     private String statuses;
     private String location;
