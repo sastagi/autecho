57d8ad1b55f7993a8dce8bf178edf1ab3dd463e6
Santosh <sastagi@gmail.com>
2016-03-22 18:48:24 -0700

    Helper classes to create accounts.

---
+++ app/src/main/java/com/autecho/account/AccountInfo.java
@@ -0,0 +1,22 @@
+package com.autecho.account;
+
+/**
+ * Created by sastagi on 3/22/16.
+ */
+public class AccountInfo {
+    /**
+     * Account type id
+     */
+    public static final String ACCOUNT_TYPE = "com.autecho.dcc";
+
+    /**
+     * Account name
+     */
+    public static final String ACCOUNT_NAME = "Autecho";
+
+    /**
+     * Auth token types
+     */
+    public static final String AUTHTOKEN = "Full access to an Autecho account";
+
+}
